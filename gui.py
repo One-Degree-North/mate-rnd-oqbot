fc27796f0deb655bee71bf2c1a9699871a402ed5
fillnye <60429866+fillnye@users.noreply.github.com>
2021-05-29 18:17:26 +0800

    Re-arranged layout and added thruster power info

--- gui.py
+++ gui.py
@@ -4,14 +4,13 @@ import PyQt5.QtMultimedia as QTM
 import PyQt5.QtMultimediaWidgets as QTMW
 import PyQt5.QtWidgets as QT
 
-
-
 app = QT.QApplication(sys.argv)
 window = QT.QWidget()
 
 window.setWindowTitle('MATE')
 window.setGeometry(0, 0, 800, 40)
 window.show()
+
 
 IMUlist = QT.QFormLayout()
 
@@ -42,18 +41,46 @@ IMUlist.addRow(QT.QLabel("Temperature:"),temperature)
 IMUBox = QT.QGroupBox("IMU")
 IMUBox.setLayout(IMUlist)
 
+
+PWMlist = QT.QFormLayout()
+
+Thruster1 = QT.QLabel()
+Thruster2 = QT.QLabel()
+Thruster3 = QT.QLabel()
+Thruster4 = QT.QLabel()
+Servo = QT.QLabel()
+
+
+Thruster1.setText(str(99.231))
+Thruster2.setText(str(99.283))
+Thruster3.setText(str(99.233))
+Thruster4.setText(str(99.231))
+Servo.setText(str(99.233))
+
+PWMlist.addRow(QT.QLabel("Thruster 1:"),Thruster1)
+PWMlist.addRow(QT.QLabel("Thruster 2:"),Thruster2)
+PWMlist.addRow(QT.QLabel("Thruster 3:"),Thruster3)
+PWMlist.addRow(QT.QLabel("Thruster 4:"),Thruster4)
+PWMlist.addRow(QT.QLabel("Servo:"),Servo)
+
+PWMBox = QT.QGroupBox("PWM")
+PWMBox.setLayout(PWMlist)
+
+
+
+
 Camera = QTM.QCamera()
 CameraView = QTMW.QCameraViewfinder()
-CameraView.show()
 Camera.setViewfinder(CameraView)
 Camera.setCaptureMode(QTM.QCamera.CaptureViewfinder)
 Camera.start()
 
+
 layout = QT.QGridLayout()
 
-layout.addWidget(QT.QLabel("Camera"),1,1)
-layout.addWidget(IMUBox,1,2)
-layout.addWidget(CameraView,1,3)
+layout.addWidget(CameraView,1,1,2,2)
+layout.addWidget(IMUBox,1,3,1,1)
+layout.addWidget(PWMBox,2,3,1,1)
 
 window.setLayout(layout)
 
