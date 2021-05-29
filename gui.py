68b51e8f74708b5f5cd4d4482fae596f0a37dc8b
fillnye <60429866+fillnye@users.noreply.github.com>
2021-05-29 17:40:48 +0800

    QTMultimedia: Added Camera Support

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import sys
 from PyQt5.QtCore import Qt
+import PyQt5.QtMultimedia as QTM
+import PyQt5.QtMultimediaWidgets as QTMW
 import PyQt5.QtWidgets as QT
+
+
 
 app = QT.QApplication(sys.argv)
 window = QT.QWidget()
@@ -17,6 +21,7 @@ zgyro = QT.QLabel()
 xaccel = QT.QLabel()
 yaccel = QT.QLabel()
 zaccel = QT.QLabel()
+temperature = QT.QLabel()
 
 xgyro.setText(str(99.233))
 ygyro.setText(str(99.231))
@@ -24,23 +29,31 @@ zgyro.setText(str(99.283))
 xaccel.setText(str(99.233))
 yaccel.setText(str(99.231))
 zaccel.setText(str(99.283))
+temperature.setText(str(99.283))
 
 IMUlist.addRow(QT.QLabel("X Rotation:"),xgyro)
 IMUlist.addRow(QT.QLabel("Y Rotation:"),ygyro)
 IMUlist.addRow(QT.QLabel("Z Rotation:"),zgyro)
 IMUlist.addRow(QT.QLabel("X Accerleration:"),xaccel)
-IMUlist.addRow(QT.QLabel("Y Accerleration"),yaccel)
-IMUlist.addRow(QT.QLabel("Z Accerleration"),zaccel)
+IMUlist.addRow(QT.QLabel("Y Accerleration:"),yaccel)
+IMUlist.addRow(QT.QLabel("Z Accerleration:"),zaccel)
+IMUlist.addRow(QT.QLabel("Temperature:"),temperature)
 
 IMUBox = QT.QGroupBox("IMU")
 IMUBox.setLayout(IMUlist)
 
+Camera = QTM.QCamera()
+CameraView = QTMW.QCameraViewfinder()
+CameraView.show()
+Camera.setViewfinder(CameraView)
+Camera.setCaptureMode(QTM.QCamera.CaptureViewfinder)
+Camera.start()
 
 layout = QT.QGridLayout()
 
 layout.addWidget(QT.QLabel("Camera"),1,1)
-
 layout.addWidget(IMUBox,1,2)
+layout.addWidget(CameraView,1,3)
 
 window.setLayout(layout)
 
