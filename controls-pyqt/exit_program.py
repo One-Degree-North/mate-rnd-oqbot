163ad401e341c511f43427f4b74eed071d75945d
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-06-05 18:02:32 +0800

    Update exit_program.py
    Delete mcu.close_serial

--- controls-pyqt/exit_program.py
+++ controls-pyqt/exit_program.py
@@ -2,11 +2,9 @@ import sys
 
 
 class Exit_Program:
-    def __init__(self, comm: Communications, mcu: MCUInterface):
+    def __init__(self, comm: Communications):
         self.comms: Communications = comm
-        self.mcu: MCUInterface = mcu
         
     def Exit(self):
         self.comms.kill_op()
-        self.mcu.close_serial()
         sys.exit()
