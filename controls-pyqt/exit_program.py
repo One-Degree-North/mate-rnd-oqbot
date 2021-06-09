8f15c4159e927a048ba589e9fac84adcdaa2da47
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-06-09 11:05:15 +0800

    Update exit_program.py
    Exits the QApplication properly

--- controls-pyqt/exit_program.py
+++ controls-pyqt/exit_program.py
@@ -2,9 +2,10 @@ import sys
 from comms import Communications
 
 class Exit_Program:
-    def __init__(self, comm: Communications):
+    def __init__(self, comm: Communications, app):
         self.comms: Communications = comm
+        self.app = app
         
     def Exit(self):
         self.comms.kill_elec_ops()
-        sys.exit()
+        sys.exit(app.exec_())
