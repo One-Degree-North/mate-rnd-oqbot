9b92172a0cc3d7af8de32b78cd6c8b1588de3def
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-06-10 16:13:00 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -17,7 +17,7 @@ class Visualizer:
         self.visual = visual
     def visualize():
         #Change to exit condition
-        while visual.running:
+        while visual.exit_program.running:
             rate(60)
             change = 0
             velocity_measurements = []
