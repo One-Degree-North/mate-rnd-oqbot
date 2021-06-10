ec88ef82df243739d2ef304e187baac1a78e2b28
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-06-10 09:12:36 +0800

    Update key_signal.py
    Added comments to explain XOR relevance and XOR ascii values of characters, instead of the characters themselves.

--- controls_pyqt/key_signal.py
+++ controls_pyqt/key_signal.py
@@ -4,8 +4,13 @@ class KeySignal:
 	def __init__(self, key: str, pressed: bool):
 		self.key: str = key
 		self.pressed: bool = pressed
+		key_ascii: int = ord(self.key)
 		
-		if self.key ^ 32 > self.key or self.key ^ 32 == 0: # self.key ^ 32 == 0 checks whether it is a space or not
+		if key_ascii ^ 32 > key_ascii or key_ascii ^ 32 == 0:
+			# key_ascii ^ 32 == 0 checks whether it is a space or not
+			# Capital and small letters in binary are distinguished by the 32-bit digit
+			# For example, A is 01000001 and a is 01100001
+			# Performing an XOR operation is one of the fastest ways to switch the case of a letter
 			self.shift = False
 		else:
 			self.shift = True
