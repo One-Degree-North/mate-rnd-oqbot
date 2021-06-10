6771e7ea716c6c85d6d7e3a120347a4d9e8216a4
arora767287 <57432791+arora767287@users.noreply.github.com>
2021-06-10 16:10:30 +0800

    Make running false

--- controls_pyqt/exit_program.py
+++ controls_pyqt/exit_program.py
@@ -5,7 +5,10 @@ from comms import Communications
 class ExitProgram:
     def __init__(self, comm: Communications):
         self.comms: Communications = comm
+        self.running = True
 
     def exit(self):
         self.comms.kill_elec_ops()
+        self.running = False
         exit()
+        
