bd88caf8f6d576640d9671f959c5c5186df524bd
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-07-11 18:02:08 +0800

    Update transect-line.py
    Added something for orientation too; it should be able to turn automatically. Instead of turning automatically using data, I can instead implement a Computer Vision-based approach too.

--- transect-line.py
+++ transect-line.py
@@ -11,10 +11,10 @@ class TransectLine:
 	def __init__(self, comms: Communications, mcu_object: MCUInterface):
     	self.comms: Communications = comms
     	self.mcu: MCUInterface = mcu_object
-		self.HEIGHT_THRESH: int = 1			# CHANGE THIS
-		self.INITIAL_ORIENTATION: int[] = self.mcu.latest_orienation
-		self.DISTANCE: int = 6				# CHANGE THIS
-		self.running = True
+	self.HEIGHT_THRESH: int = 1				# CHANGE THIS
+	self.ORIENTATION_THRESH: int = 5		# CHANGE THIS
+	self.DISTANCE: int = 6					# CHANGE THIS
+	self.running = True
 			
 	def move_forward(self):
 		trigger: str = "w"
@@ -37,19 +37,45 @@ class TransectLine:
 		self.comms.read_send(KeySignal(trigger, pressed))
 		self.running = False
 		
+	def move(self, time_needed: float, trigger: str):
+		pressed: bool = True
+		self.comms.read_send(KeySignal(trigger, pressed))
+		time.sleep(time_needed)
+		pressed = False
+		self.comms.read_send(KeySignal(trigger, pressed))
+	
+	# Can take CV approach too
 	def adjust_orientation(self):
+		left_trigger: str = "a"
+		right_trigger: str = "d"
+		starttime = time.time()
+			
 		while self.running:
-			### DO SOMETHING
-		
+			time_now = time.time()
+			time_diff: float = time_now - start_time
+			start_time = time_now
+			
+			TURN_INDEX: int = 0
+			angvel: float = self.mcu.latest_orientation[TURN_INDEX]
+			self.orientation = self.orientation + angvel * time_diff
+			
+			if abs(self.orientation) > self.ORIENTATION_THRESH:
+				time_needed: float = abs(self.orientation) / angvel
+					
+				if self.orientation > 0:
+					self.move(time_needed, left_trigger)
+				elif self.orientation < 0:
+					self.move(time_needed, right_trigger)
+					
 	def adjust_height(self):
 		up_trigger: str = "e"				# CHANGE THIS
 		down_trigger: str = "q"				# CHANGE THIS
-		starttime = time.time()
+		start_time = time.time()
 		self.height: float = 0
 			
 		while self.running:
-			timenow = time.time()
-			time_diff: float = timenow - starttime
+			time-now = time.time()
+			time_diff: float = time_now - start_time
 			starttime = timenow
 			
 			DOWN_AXIS_INDEX: int = 1		# CHANGE THIS
@@ -57,26 +83,14 @@ class TransectLine:
 			self.height = self.height + 0.5 * self.accel * (time_diff ** 2)
 			
 			if abs(self.height) > self.HEIGHT_THRESH:
-				timeneeded: float = (2 * abs(self.height / accel)) ** 0.5
+				time_needed: float = (2 * abs(self.height / accel)) ** 0.5
 				
 				if self.height > 0:			# Positive height, positive acceleration = higher, going up
-					pressed: bool = True
-					self.comms.read_send(KeySignal(down_trigger, pressed))
-					time.sleep(timeneeded)
-					pressed = False
-					self.comms.read_send(KeySignal(down_trigger, pressed))
+					self.move(time_needed, down_trigger)
 				elif self.height < 0:		# Negative height, negative acceleration = deeper, going down
-					pressed: bool = True
-					self.comms.read_send(KeySignal(up_trigger, pressed))
-					time.sleep(timeneeded)
-					pressed = False
-					self.comms.read_send(KeySignal(up_trigger, pressed))
+					self.move(time_needed, up_trigger)
 				
   	def navigate(self):
-    	#x_orientation: int = self.mcu.latest_orientation[0]
-    	#y_orientation: int = self.mcu.latest_orientation[1]
-		#z_orientation: int = self.mcu.latest_orientation[2]
-			
 		self.forward_thread = threading.Thread(target = self.move_forward)
 		self.forward_thread.start()
 		self.adjust_orientation_thread = threading.Thread(target = self.adjust_orientation)
