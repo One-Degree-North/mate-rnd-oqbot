f66f5f58500616e7c862f70bfbb015033afc113a
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-07-13 14:55:21 +0800

    Update transect-line.py
    Contours utilized, sorted by area, program knows when it's too high or too low.

--- transect-line.py
+++ transect-line.py
@@ -5,11 +5,21 @@ class TransectLine:
 		self.camera = cv2.VideoCapture(camera_id)
 		self.BLUE_LOWER: (int, int, int) = blue_lower		# INPUT BLUE IN HSV
 		self.BlUE_UPPER: (int, int, int) = blue_upper
+		self.WIDTH = camera.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+		self.HEIGHT = camera.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+		self.RATIO_UPPER_BOUND = 0.8
+		self.RATIO_LOWER_BOUND = 0.6
 		
 		if self.camera.isOpened():
 			print("Footage opened!")
 		
-	def run():
+	def move_up(self):
+		# Do something
+		
+	def move_down(self):
+		# Do something
+		
+	def run(self):
 		running = True
 		while running: 
 			_, frame = camera.read()
@@ -26,4 +36,16 @@ class TransectLine:
 			contour_one = contours[0]
 			contour_two = contours[1]
 			
-			# Find the distance between them
+			x1: int, _, w1: int, _ = cv2.boundingRect(contour_one)
+			x2: int, _, w2: int, _ = cv2.boundingRect(contour_two)
+			
+			centre_c1 = x1 + w1/2
+			centre_c2 = x2 + w2/2
+			
+			ratio = abs(centre_c1 - centre_c2) / self.WIDTH
+			
+			if ratio >= self.RATIO_UPPER_BOUND:
+				self.move_up()
+			
+			if ratio <= self.RATIO_LOWER_BOUND:
+				self.move_down()
