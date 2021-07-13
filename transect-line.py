5364a8ef68c6cb1eb80e7a7323dee5bbc389db92
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2021-07-13 19:10:10 +0800

    Update transect-line.py
    Connected with Microcontroller

--- transect-line.py
+++ transect-line.py
@@ -1,13 +1,48 @@
 import cv2
+import sys
 
 RATIO_UPPER_BOUND = 0.8
 RATIO_LOWER_BOUND = 0.6
 
+BAUD_RATE: int = 230400
+CLOSE_ON_STARTUP: bool = True
+MAX_READ: int = 16
+
+port_list = list_ports.comports()
+if not port_list:
+    print("No available tty/COM ports. Halting!")
+    exit()
+port_list = [x.device for x in port_list]
+if len(port_list) == 1:
+    PORT = port_list[0]
+    print(f"Using port {PORT}")
+else:
+    print(f"List of available tty/COM ports: {port_list}")
+    PORT = input("Port to use: ")
+    if len(PORT) == 0:
+        PORT = "/dev/ttyUSB0"
+
+REFRESH_RATE: int = 1440
+
+INITIAL_PERCENTAGE: int = 25
+SENSITIVE_PERCENTAGE: int = 15
+	
+feather = mcu.MCUInterface(PORT,
+					   baud=BAUD_RATE,
+					   close_on_startup=CLOSE_ON_STARTUP,
+					   refresh_rate=REFRESH_RATE,
+					   max_read=MAX_READ)
+communications = comms.Communications(feather, SENSITIVE_PERCENTAGE, INITIAL_PERCENTAGE)
+
 def move_up():
-	# Do something
+	comms.up(SENSITIVE_PERCENTAGE)
+	time.sleep(1)
+	comms.up(0)
 
 def move_down():
-	# Do something
+	comms.down(SENSITIVE_PERCENTAGE)
+	time.sleep(1)
+	comms.down(0)
 	
 def ended(frame):
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -40,6 +75,10 @@ def run(camera_id: int, blue_lower: (int, int, int), blue_upper: (int, int, int)
 	
 	if camera.isOpened():
 		print("Footage opened!")
+		comms.forward(INITIAL_PERCENTAGE)
+	else:
+		print("Error: camera not found!"
+		break
 
 	# start moving
 	running = True
@@ -66,11 +105,18 @@ def run(camera_id: int, blue_lower: (int, int, int), blue_upper: (int, int, int)
 		x2: int, _, w2: int, _ = cv2.boundingRect(contour_two)
 
 		ratio_check(x1, w1, x2, w2)
-
 	# stop moving
 
 if __name__ == "__main__":
+	communications.start_elec_ops()
+	communications.start_thread()
+	
 	camera_id: int = 0
 	blue_lower: (int, int, int) = ( , , )
 	blue_upper: (int, int, int) = ( , , )
+		
 	run(camera_id, blue_lower, blue_upper)
+	
+	communications.kill_elec_ops()
+	sys.exit()
+	# Movement and HSV thresholds
